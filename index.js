79f1f064bd9bd53c33a39e6aadf058e251b1b95a
Karen Valier <karenvalierpalmero@gmail.com>
2022-09-23 03:02:56 -0500

    agregue mas funciones a el archivo index.js y styles.css

--- index.js
+++ index.js
@@ -8,12 +8,36 @@ function Submit (){
     if(listaTareas === null){
         listaTareas = [];
     }
+    validateForm();
+   
+}
+
+//Validar
+function validateForm(){
+
+    let tarea = document.getElementById('tarea-area').value;
+    let desc = document.getElementById('desc-area').value;
+    let fecha = Date.parse(document.getElementById('fecha-area').value);
+
+    if(tarea.length == 0){
+        alert('Escribe un nombre para la tarea');
+        return false;
+    }else if(desc.length == 0)
+    {
+        alert('Escribe una descripcion para la tarea');
+        return false;
+    
+    }else if(isNaN(fecha))
+    {
+        alert('Elige la fecha para la tarea');
+        return false;
+    }
     createData();
+
 
 }
 
 //Create
-//Obtenemos la información desde el Formulario
 function createData(){
     const nuevaTarea = {
         tarea : document.getElementById('tarea-area').value,
@@ -27,19 +51,18 @@ function createData(){
         
         listaTareas.push(nuevaTarea);
         localStorage.setItem('listTareas', JSON.stringify(listaTareas));
-        showData(listaTareas);
-
-
+        showData();
 }
 
 //Read
 function showData(){
-     listaTareas = JSON.parse(localStorage.getItem('listTareas'));
+    listaTareas = JSON.parse(localStorage.getItem('listTareas'));
      document.getElementById('tableBody').innerHTML = "";
-     if(listaTareas !== null){
+      if(listaTareas !== null){
         listaTareas.forEach((element,index)=>{
             let row = document.createElement('tr');
             row.innerHTML = `
+            <td scope="row">${index+1}</td>
             <td>${element.tarea}</td>
             <td>${element.descripcion}</td>
             <td>${element.fecha}</td>
@@ -51,25 +74,49 @@ function showData(){
             document.getElementById('tableBody').appendChild(row);
         });
      }
-    }
+}
 //Edit
-function editItem(tableData){
-
-
-
-
+function editItem(index){
+        document.getElementById('numTarea').value = index;
+        document.getElementById('tarea-area').value = listaTareas[index].tarea;
+        document.getElementById('desc-area').value = listaTareas[index].descripcion;
+        document.getElementById('fecha-area').value = listaTareas[index].fecha;
 }
 
 //Update
-
 function updateData(){
+    let posicion = document.getElementById('numTarea').value;
+    let tareaActual = document.getElementById('tarea-area').value;
+    let descActual = document.getElementById('desc-area').value;
+    let fechaActual = document.getElementById('fecha-area').value;
+
+    
+    if(posicion == '' || isNaN(posicion)){
+        alert('Seleccione un registro');
+        return false;
+    }
+    
+    
+    listaTareas[posicion]={
+        tarea : tareaActual,
+        descripcion: descActual,
+        fecha : fechaActual
+    }
+    localStorage.setItem('listTareas', JSON.stringify(listaTareas));
+
+    document.getElementById('numTarea').value = '';
+    document.getElementById('tarea-area').value = '';
+    document.getElementById('desc-area').value = '';
+    document.getElementById('fecha-area').value = ''
+    showData();
 
 }
 
 //Delete
-
-function deleteItem(){
-
+function deleteItem(indexActual){
+    listaTareas = listaTareas.filter((alumno, index)=> index !== indexActual);
+    localStorage.setItem('listTareas', JSON.stringify(listaTareas));
+    showData();
 }
 
-showData(listaTareas);
+showData();
